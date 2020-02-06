6349d65945cda7c2a11719fd6bf7c6302d4de5b1
Vladimir Perfilev <persoff68@gmail.com>
2020-02-06 16:25:06 +0100

    rename base classes

--- src/main/java/com/persoff68/fatodo/FaToDoAuthServiceApplication.java
+++ src/main/java/com/persoff68/fatodo/FaToDoAuthServiceApplication.java
@@ -1,18 +1,16 @@
 package com.persoff68.fatodo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
 @SpringBootApplication(exclude = {
         UserDetailsServiceAutoConfiguration.class
 })
-public class ExtendedSkeletonApplication {
+public class FaToDoAuthServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ExtendedSkeletonApplication.class, args);
+        SpringApplication.run(FaToDoAuthServiceApplication.class, args);
     }
 
 }
