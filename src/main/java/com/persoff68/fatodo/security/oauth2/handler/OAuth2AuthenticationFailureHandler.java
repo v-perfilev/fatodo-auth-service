5c4f3553dc95559b428d839323195146faf00cd7
Vladimir Perfilev <persoff68@gmail.com>
2023-01-10 18:12:47 +0100

    TDM-189 oauth2 redirect even if error

--- src/main/java/com/persoff68/fatodo/security/oauth2/handler/OAuth2AuthenticationFailureHandler.java
+++ src/main/java/com/persoff68/fatodo/security/oauth2/handler/OAuth2AuthenticationFailureHandler.java
@@ -1,11 +1,14 @@
 package com.persoff68.fatodo.security.oauth2.handler;
 
+import com.persoff68.fatodo.security.exception.OAuth2InternalException;
 import com.persoff68.fatodo.security.oauth2.repository.CookieAuthorizationRequestRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,8 +24,18 @@ public class OAuth2AuthenticationFailureHandler extends SimpleUrlAuthenticationF
     public void onAuthenticationFailure(HttpServletRequest request,
                                         HttpServletResponse response,
                                         AuthenticationException exception) {
+        String redirectUri = getRedirectFromCookies();
         cookieAuthorizationRequestRepository.removeAuthorizationRequestCookies(request, response);
         log.warn("OAuth2 authentication failed: {}", exception.getMessage());
+        throw new OAuth2InternalException(redirectUri);
+    }
+
+    private String getRedirectFromCookies() {
+        ServletRequestAttributes requestAttributes =
+                (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        return requestAttributes != null
+                ? cookieAuthorizationRequestRepository.loadRedirect(requestAttributes.getRequest())
+                : null;
     }
 
 }
